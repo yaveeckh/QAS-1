efd6eb6d4088cb661224a319963add0acb939e5a
Yanis Van Eeckhout <yaveeckh@w217h058.wireless.ugent.be>
2024-10-21 15:19:09 +0200

    CHI2

--- ziggurat.py
+++ ziggurat.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.stats import norm
+from scipy.stats import norm, chi2
 from scipy.optimize import minimize, Bounds
 import matplotlib.pyplot as plt 
 
@@ -41,6 +41,24 @@ def tail_normal(x1):
     else:
         return tail_normal(x1)
     
+def c2test(N, k, alpha):
+    x = belly_and_tail_normal(1, N)
+    Nj, edges = np.histogram(x, bins=k, range=[0, 8])
+    expected = np.zeros(k)
+
+    for i in range(k):
+        expected[i] = (cdf(edges[i+1]) - cdf(edges[i])) * N
+
+    X2 = sum((xj-expected[j])**2/(expected[j]) for (j,xj) in enumerate(Nj))
+
+    print(X2)
+    print(chi2.ppf(alpha, k-1))
+    if X2 < chi2.ppf(alpha, k-1):
+        print("Passed")
+    else:
+        print("Failed")
+
+    
 def plot_distribution(N):
     x1 = 1
     x = belly_and_tail_normal(x1, N)
@@ -52,26 +70,32 @@ def plot_distribution(N):
 
     plt.show()
 
-    a = 1/(x1*pdf(0)) * cdf(x1)
-    b = np.log(x1) + (x1**2/2) + 1/2* np.log(2*np.pi) + np.log(1-cdf(x1)/2)
-    print(a)
-    print(b)
-
 def plot_success():
 
-    x1 = np.linspace(0.01, 7, 1000)
+    x1 = np.linspace(0.1, 7, 1000)
     p = lambda x : 1/(x*pdf(0)) * cdf(x)
     q = lambda x: x * np.exp(x**2/2) * np.sqrt(2*np.pi) * (1-norm.cdf(x))
     En = lambda x: cdf(x) * 2 * 1/p(x) + (1-cdf(x)) * 2 * 1/q(x) + 1
     En_min = minimize(En, 1, bounds=Bounds(0, 10))
     print("Minimum amount amount of uniform rvs at x1 = {} and E[N] = {}".format(En_min.x, En_min.fun))
     
-    plt.plot(x1,p(x1))
-    plt.plot(x1,q(x1))
-    plt.plot(x1,En(x1))
-    
+    plt.plot(x1,p(x1), label="$p(x_1)$")
+    plt.plot(x1,q(x1), label="$q(x_1)$")
+    plt.xlabel("$x_1$")
+    plt.legend()
+    plt.title("Succes probabilities of one trial in belly and tail")
     plt.show()
+
+    plt.plot(x1,En(x1), label="$E_{x_1}[N]$")
+    plt.xlabel("$x_1$")
+    plt.legend()
+    plt.title("Mean number of required uniform samples per half-normal sample")
+    plt.show()
+
+
+    
     
 if __name__ == "__main__":
-    #plot_distribution(1000)
-    plot_success()
+   #plot_distribution(100000)
+    #plot_success()
+    c2test(5, 10, 0.95)
